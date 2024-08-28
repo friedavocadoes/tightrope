05fac0db1e65b1d7bcea109601f705d862022116
12208155 <choudharyshreya121@gmail.com>
2024-08-29 01:38:21 +0530

    shreya

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
     return (
+        <>
         <Navbar />
+        <div>
+            <Signup />
+        </div>
+        <div>
+            <Login />
+        </div>
+        
+        </>
     )
 }
 
